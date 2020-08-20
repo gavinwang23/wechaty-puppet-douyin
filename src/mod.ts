999b099710a750ac7249b9fa0633f5082a5c8914
王俊伟 <wangjunwei@wangjunweide-MacBook-Pro.local>
2020-08-20 23:46:51 +0800

    douyin puppet update

--- src/mod.ts
+++ src/mod.ts
@@ -1,11 +1,3 @@
-/*
- * @Descripttion: 
- * @version: 
- * @Author: wangjunwei
- * @Date: 2020-07-28 00:45:21
- * @LastEditors: wangjunwei
- * @LastEditTime: 2020-08-20 23:17:17
- */
 import { VERSION }    from './version'
 import { PuppetDouyin } from './puppet-douyin'
 
